3a1c5c0f85f4648c05277e9390dd67697ed7d65d
Michela <michelaziemer@gmail.com>
2022-08-24 19:21:56 -0500

    Changed latlongs to locations

--- Michela_Web/APP/static/js/logic.js
+++ Michela_Web/APP/static/js/logic.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "300px";
+    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
@@ -13,6 +13,10 @@ function openNav() {
 
 
 
+  
+
+
+
 $(document).ready(function() {
     console.log("Page Loaded");
 
@@ -20,11 +24,32 @@ $(document).ready(function() {
         // alert("button clicked!");
         makePredictions();
     });
+    let locations = {"North Pole": [90.00, -135.00], "South Pole": [-90.00, 45.00], "Mariana Trench":[11.3493, 142.1996],
+    "Bermuda Triangle":[25.0000, -71.0000], "Dead Sea": [31.5590, 35.4732], "Grand Canyon":[36.0544, -112.1401],
+    "Great Barrier Reef": [-18.2871, 147.6992], "Great Wall of China": [40.4319, 116.5704], "Eiffel Tower": [48.8584, 2.2945],
+    "The Great Pyramid of Giza":[29.9792, 31.1342]}
+        
+        for (let key in locations) {
+        var lat = locations[key][0]
+        $('#select').append('<option value=' + locations[key] + '>' + key + '</option>');
+        
+        }
+         
+        
+
 });
+
 
 
 // call Flask API endpoint
 function makePredictions() {
+    var location = $("#select").val();
+    const myArray = location.split(",");
+    const lat = parseFloat(myArray[0]);
+    const long = parseFloat(myArray[1]);
+    console.log(lat)
+    console.log(long)
+
     var BusinessAcceptsCreditCards = $("#BusinessAcceptsCreditCards").val();
     var GoodForKids = $("#GoodForKids").val();
     var WheelchairAccessible = $("#WheelchairAccessible").val();
@@ -32,11 +57,14 @@ function makePredictions() {
     var BusinessParkingTypeId = $("#BusinessParkingTypeId").val();
     var RestaurantsTypeId = $("#RestaurantsTypeId").val();
     var GoodForMealTypeId = $("#GoodForMealTypeId").val();
-    var latitude = $("#latitude").val();
-    var longitude = $("#longitude").val();
+    var latitude = lat;
+    var longitude = long;
+    
+    // var latitude = $("#latitude").val();
+    // var longitude = $("#longitude").val();
 
     // check if inputs are valid
-
+    //
     // create the payload
     var payload = {
         "BusinessAcceptsCreditCards": BusinessAcceptsCreditCards,
@@ -50,6 +78,8 @@ function makePredictions() {
         "longitude": longitude
     }
 
+
+
     // Perform a POST request to the query URL
     $.ajax({
         type: "POST",
@@ -59,18 +89,13 @@ function makePredictions() {
         success: function(returnedData) {
             // print it
             console.log(returnedData);
-            console.log(Object.values(returnedData))
-        var test = Object.entries(returnedData[0]['result'])
+
         var result = []
-
-
         Object.entries(returnedData[0]).forEach(([key, value]) => {result.push(value)});
         $("#output").text(`We're ${result[0]}% sure your restaurant would get a ${result[1]}-star rating on Yelp!`); 
-
+           
          
 
-        
-    
 
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
